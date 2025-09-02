b3cd03f69ba3f498bde269f67fd83ade506a23c0
Brwniecv <diegomacias2032@gmail.com>
2025-09-02 10:00:59 -0500

    Add files via upload

---
+++ First Repository.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> This is the Boiler Plate template</title>
+
+<link rel="stylesheet" type="text/css" href="include/stylesheet.css"
+<script src="include/script.js"></script>
+</head>
+<body>
+<p style="text-align:center;"> This was updated in github</p>
+</body>
+</html>
